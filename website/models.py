a92b2bb07b67c1ebe05f1e1a2ece8e443e6fdf55
Dagemtef <dagem.teferra@bison.howard.edu>
2021-11-22 21:55:43 -0500

    DT edits

--- website/models.py
+++ website/models.py
@@ -1,12 +1,5 @@
 from . import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
-
-class Offset(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    trees = db.Column(db.Integer)
-    money = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id '))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,4 +8,4 @@ class User(db.Model, UserMixin):
     first_Name = db.Column(db.String(150))
     last_Name = db.Column(db.String(150))
     co2emissions = db.Column(db.Integer)
-    offsets = db.relationship('Offset')
+    #offsets = db.Column(db.String(1000))
