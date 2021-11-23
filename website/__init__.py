0bd6bbc110f9daa873215bc96e7e1edda518671a
Dagemtef <dagem.teferra@bison.howard.edu>
2021-11-22 21:55:53 -0500

    DT edits

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -17,9 +19,17 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
-    from .models import User, Offset
+    from .models import User
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app 
 
